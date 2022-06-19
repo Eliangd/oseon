0ec997934a4c3c8574ca44c84a06d14bb5c70446
Eliangd <eliangdanieli@gmail.com>
2022-06-19 02:44:40 -0300

    alçteracoes config

--- routes/web.php
+++ routes/web.php
@@ -11,23 +11,29 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 //Rotas Públicas
+
+//Rota tela inicial do site.
 Route::get('/', function () {
     return view('welcome');
 });
+//Rota busca/informações da ordem.
 Route::get('search', [BuscaController::class, 'pesquisa']);
 
-//Rotas Admin
-Auth::routes();
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-Route::resource('usuario', UsersController::class);
-Route::resource('ordem', OrdensController::class);
 
-//Rotas reponsáveis pelo envio de e-mail
+//Rotas Admin
+
+//Usuário precisa estar logado no sistema para acessar as rotas.
+Auth::routes(); 
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home'); //Rota tela home/Seleção dos módulos do site.
+Route::resource('usuario', UsersController::class); //Rotas Usuário.
+Route::resource('ordem', OrdensController::class); //Rotas Ordem.
+
+//Rotas reponsáveis pelo envio de e-mail.
 Route::get('/enviarEmail', [EmailController::class, 'email']);
 Route::post('/enviarEmail', [App\Http\Controllers\EmailController::class, 'email']);
-
 Route::get('/email', [App\Http\Controllers\EmailController::class, 'index']);  //retorna a tela de e-mail enviado com sucesso.
 
+//Rotas responsáveis pelo filtro de ordens.
 Route::post('/filtro', [App\Http\Controllers\OrdensController::class, 'filtroOrdens'])->name('filtro');
 Route::get('/filtro', [App\Http\Controllers\OrdensController::class, 'filtroOrdens'])->name('filtro'); 
 //Adicionado essa rota GET para funcionar a ordenação após filtrar
