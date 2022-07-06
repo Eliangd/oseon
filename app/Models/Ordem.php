45efbc1f40b6875c059c16ed47278a215ff454ef
Eliangd <eliangdanieli@gmail.com>
2022-07-05 21:34:54 -0300

    att

--- app/Models/Ordem.php
+++ app/Models/Ordem.php
@@ -13,7 +13,7 @@ class Ordem extends Model
     protected $fillable = ['id', 'ords_codigo', 'protocolo', 'nome', 'email', 'telefone', 'cpf', 'endereco', 'equipamento', 'modelo', 'acessorios', 'defeito', 'relatorio', 'status_ordem', 'data_abertura'];
     //define que esses atributos podem ser armazenados em massa no banco.
 
-    public $sortable = ['ords_codigo', 'protocolo', 'nome', 'status_ordem'];
+    public $sortable = ['id', 'protocolo', 'nome', 'status_ordem'];
     // define a ordenação para esses campos.
 
 }
